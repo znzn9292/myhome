17a481d524391f0ca709972f6163e67bb197d1a4
jungsunhwan <znzn9292@gmail.com>
2021-01-23 23:59:53 +0900

    # AWS RDS DB 정보 변경

---
+++ src/main/java/com/znzn/mythymeleaf/ServletInitializer.java
@@ -0,0 +1,14 @@
+package com.znzn.mythymeleaf;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MythymeleafApplication.class);
+    }
+
+}
